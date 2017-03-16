0873835bfb2af974f0aa75c04654c904c371b159
rzippo <raffaele.zippo@live.it>
2017-03-16 20:03:45 +0100

    Code versions designed and tested

---
+++ NoThreadsYesVectorization/main.cpp
@@ -0,0 +1,56 @@
+#include <cstdlib>
+#include <ctime>
+
+#include <fstream>
+
+using namespace std;
+
+void matrixComputation(float **A, float **B, int size)
+{
+	for (int i = 0; i < 10; i++)
+	{
+		for (int i = 0; i < size; i++)
+		{
+			for (int j = 0; j < size; j++)
+			{
+				A[i][j] += B[i][j];
+			}
+		}
+	}	
+}
+
+int main(int argc, char* argv[])
+{
+	int size = atoi(argv[1]);
+
+	float **A = new float*[size];
+	float **B = new float*[size];
+
+	for (int i = 0; i < size; i++)
+	{
+		A[i] = new float[size];
+		B[i] = new float[size];
+
+		for (int j = 0; j < size; j++)
+		{
+			A[i][j] = rand() * (float)(1 << 15);
+			B[i][j] = rand() * (float)(1 << 15);
+		}
+	}
+
+	ofstream txt("results_nTyV.txt");
+	double total_time = 0;
+	int times = 15;
+	
+	for (int i = 0; i < times; i++)
+	{
+		clock_t start = clock();
+		matrixComputation(A, B, size);
+		double duration = (clock() - start) / ((double)CLOCKS_PER_SEC);
+		total_time += duration;
+		
+		txt << duration << endl;
+	}	
+
+	txt << endl << "Mean: " << total_time / times << endl;
+}
---
+++ YesThreadNoVectorization/main.cpp
@@ -0,0 +1,72 @@
+#include <cstdlib>
+#include <ctime>
+#include <fstream>
+#include <thread>
+
+using namespace std;
+
+void matrixComputationWorker(float **A, float **B, int size)
+{
+	#pragma loop(no_vector)
+	for (int i = 0; i < 10; i++)
+	{
+		#pragma loop(no_vector)
+		for (int i = 0; i < size; i++)
+		{
+			#pragma loop(no_vector)
+			for (int j = 0; j < size; j++)
+			{
+				A[i][j] += B[i][j];
+			}
+		}
+	}
+}
+
+void matrixComputation(float **A, float **B, int size)
+{
+	thread t1(matrixComputationWorker, A, B, size / 2);
+	thread t2(matrixComputationWorker, A + size / 2, B, size / 2);
+	thread t3(matrixComputationWorker, A, B + size / 2, size / 2);
+	thread t4(matrixComputationWorker, A + size / 2, B + size / 2, size / 2);
+
+	t1.join();
+	t2.join();
+	t3.join();
+	t4.join();
+}
+
+int main(int argc, char* argv[])
+{
+	int size = atoi(argv[1]);
+
+	float **A = new float*[size];
+	float **B = new float*[size];
+
+	for (int i = 0; i < size; i++)
+	{
+		A[i] = new float[size];
+		B[i] = new float[size];
+
+		for (int j = 0; j < size; j++)
+		{
+			A[i][j] = rand() * (float)(1 << 15);
+			B[i][j] = rand() * (float)(1 << 15);
+		}
+	}
+
+	ofstream txt("results_yTnV.txt");
+	double total_time = 0;
+	int times = 15;
+
+	for (int i = 0; i < times; i++)
+	{
+		clock_t start = clock();
+		matrixComputation(A, B, size);
+		double duration = (clock() - start) / ((double)CLOCKS_PER_SEC);
+		total_time += duration;
+
+		txt << duration << endl;
+	}
+
+	txt << endl << "Mean: " << total_time / times << endl;
+}
